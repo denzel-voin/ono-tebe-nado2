6c1590893ad58d0de72ff837934fb2305cbb3794
Denis Voinovich <denzelvojn@gmail.com>
2024-08-08 14:08:01 +0300

    done view of basket, order and refactor

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,40 +1,55 @@
 export type LotStatus = 'wait' | 'active' | 'closed';
 
 export interface IAuction {
-    status: LotStatus;
-    datetime: string;
-    price: number;
-    minPrice: number;
-    history?: number[];
+	status: LotStatus;
+	datetime: string;
+	price: number;
+	minPrice: number;
+	history?: number[];
 }
 
 export interface ILotItem {
-    id: string;
-    title: string;
-    about: string;
-    description?: string;
-    image: string;
+	id: string;
+	title: string;
+	about: string;
+	description?: string;
+	image: string;
 }
 
 export type ILot = ILotItem & IAuction;
 
-export type LotUpdate = Pick<ILot, 'id' | 'datetime' | 'status' | 'price' | 'history'>;
+export type LotUpdate = Pick<
+	ILot,
+	'id' | 'datetime' | 'status' | 'price' | 'history'
+>;
+
+export type IBasketItem = Pick<ILot, 'id' | 'title' | 'price'> & {
+	isMyBid: boolean;
+};
+
+export interface IAppState {
+	catalog: ILot[];
+	basket: string[];
+	preview: string | null;
+	order: IOrder | null;
+	loading: boolean;
+}
 
 export interface IOrderForm {
-    email: string;
-    phone: string;
+	email: string;
+	phone: string;
 }
 
 export interface IOrder extends IOrderForm {
-    items: string[]
+	items: string[];
 }
 
 export type FormErrors = Partial<Record<keyof IOrder, string>>;
 
 export interface IBid {
-    price: number
+	price: number;
 }
 
 export interface IOrderResult {
-    id: string;
+	id: string;
 }
